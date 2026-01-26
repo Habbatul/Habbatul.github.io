cf6372d26c0b13f50b8c5f584298a4d6546f67e4
Habbatul <gamernolifeneet@gmail.com>
2026-01-27 02:12:46 +0700

    refactor: try vercell build script

--- api/notion-webhook.js
+++ api/notion-webhook.js
@@ -1,24 +1,35 @@
 import axios from "axios";
-import { fetchNotionArticles } from "../scripts/fetch-notion-afterbuild.js";
+// import { fetchNotionArticles } from "../scripts/fetch-notion-afterbuild.js";
 
 const DEPLOY_HOOK = process.env.DEPLOY_HOOK;
 
 export default async function handler(req, res) {
-    // if (req.method !== "POST") {
-    //     return res.status(405).json({ error: "Method not allowed" });
-    // }
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
 
     // Fire-and-forget Notion fetch
-    fetchNotionArticles().catch(err => console.error(err));
+    // fetchNotionArticles().catch(err => console.error(err));
+
+    let body = '';
+
+    try {
+        body = req.body;
+        console.log('Body:', body);
+    } catch (e) {
+        console.log('No body or failed to parse body');
+    }
 
     // Trigger Vercel deploy hook dengan axios
-    // if (DEPLOY_HOOK) {
-    //     axios.post(DEPLOY_HOOK)
-    //         .then(() => console.log("✅ Vercel deploy triggered"))
-    //         .catch(err => console.error("❌ Deploy hook failed:", err.message));
-    // } else {
-    //     console.warn("⚠️ DEPLOY_HOOK not defined");
-    // }
+    if (DEPLOY_HOOK) {
+        axios.post(DEPLOY_HOOK)
+            .then(() => console.log("✅ Vercel deploy triggered"))
+            .catch(err => console.error("❌ Deploy hook failed:", err.message));
+    } else {
+        console.warn("⚠️ DEPLOY_HOOK not defined");
+    }
 
     return res.status(200).json({ ok: true });
 }
+
+
